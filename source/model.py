ac677b0f62f6cbc2f2f3224b30aa9d0ff6a6f768
Apoorv Jha <11380@exchange.isb.edu>
2024-11-02 10:37:24 +0530

    Updated the runtime params.

--- source/model.py
+++ source/model.py
@@ -11,17 +11,17 @@ class ObjectDetectionCNN(nn.Module):
         self.input_channels = input_channels
         self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=input_channels * 8, kernel_size=(3,3), padding="same")
         self.conv2 = nn.Conv2d(in_channels=input_channels * 8, out_channels=input_channels * 16, kernel_size=(3,3), padding="same")
-        self.pool1 = nn.MaxPool2d(kernel_size = (2,2))
+        self.pool1 = nn.MaxPool2d(kernel_size = (2,2)) # 224 X 224 -> 112 X 112
         self.conv3 = nn.Conv2d(in_channels=input_channels * 16, out_channels=input_channels * 32, kernel_size=(3,3), padding="same")
         self.conv4 = nn.Conv2d(in_channels=input_channels * 32, out_channels=input_channels * 64, kernel_size=(3,3), padding="same")
-        self.pool2 = nn.MaxPool2d(kernel_size = (2,2))
+        self.pool2 = nn.MaxPool2d(kernel_size = (2,2)) # 112 X 112 -> 56 X 56
         self.conv5 = nn.Conv2d(in_channels=input_channels * 64, out_channels=input_channels * 128, kernel_size=(3,3), padding="same")
         self.conv6 = nn.Conv2d(in_channels=input_channels * 128, out_channels=input_channels * 256, kernel_size=(3,3), padding="same")
-        self.pool3 = nn.MaxPool2d(kernel_size = (2,2))
+        self.pool3 = nn.MaxPool2d(kernel_size = (2,2)) # 56 X 56 -> 28 X 28
         self.conv7 = nn.Conv2d(in_channels=input_channels * 256, out_channels=input_channels * 512, kernel_size=(3,3), padding="same")
         self.conv8 = nn.Conv2d(in_channels=input_channels * 512, out_channels=input_channels * 1024, kernel_size=(3,3), padding="same")
-        self.pool4 = nn.MaxPool2d(kernel_size = (2,2))
-        self.fc1 = nn.Linear(1024 * 8 * 8, 1024)
+        self.pool4 = nn.MaxPool2d(kernel_size = (2,2)) # 28 X 28 -> 14 X 14
+        self.fc1 = nn.Linear(1024 * 14 * 14, 1024)
         self.fc2 = nn.Linear(1024, 256)
         self.fc_cls = nn.Linear(256, n_classes)
         self.fc3 = nn.Linear(256, 64)
